86211615edfb1ed49a6751a3aa8072b6c6bafc44
Alexey Beschetnikoff <bronzspawn@gmail.com>
2023-02-16 22:20:28 +0700

    make class refactoring for whole app

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
 'use strict';
 
-const fishList = [];
-const maxFish = 10;
-const field = {
-    timer: document.getElementById('timer'),
-    username: document.getElementById('username'),
-    userscore: document.getElementById('userscore'),
-    pool: document.getElementById('pool'),
-    pool_W: document.getElementById('pool').clientWidth,
-    pool_H: document.getElementById('pool').clientHeight,
-};
-
-function createFish(n) {
-    for(let i = 1; i <= n; i++) {
-        let fishNode = document.createElement('div');
-        fishNode.classList.add('fish');
-        fishNode.style.top = (Math.random() * field.pool_H - fishNode.clientWidth) + 'px';
-        fishNode.style.left = (Math.random() * field.pool_W - fishNode.clientHeight) + 'px';
-        fishList.push(fishNode);
-        field.pool.append(fishNode);
-        fishNode.onclick = ()=>{
-            fishNode.remove();
+class fishingGame {
+    constructor(maxTimer = 10000, maxFish = 10) {
+        this.maxFish = maxFish;
+        this.maxTimer = maxTimer;
+        this.fishList = [];
+        this.field = {
+            timer: document.getElementById('timer'),
+            username: document.getElementById('username'),
+            userscore: document.getElementById('userscore'),
+            pool: document.getElementById('pool'),
+            pool_W: document.getElementById('pool').clientWidth,
+            pool_H: document.getElementById('pool').clientHeight,
         };
-
-        console.log(fishNode);
-        console.log(fishList);
     }
-    return fishList;
+    start() {
+        this.fishList = [...this.createFish(3)];
+        this.user = {
+            name: prompt('Введите имя пользователя'),
+            score: 0,
+        }
+        let time = this.maxTimer;
+
+        this.field.username.innerHTML = `Имя: ${this.user.name ? this.user.name : 'неизвестный'}`;
+
+        const gameInterval = setInterval(() => {
+            time -= 1000;
+            this.field.timer.innerHTML = `До конца: ${time / 1000} сек.`;
+            this.fishList.push(...this.createFish(3));
+            
+            if(time < 0) {
+                clearInterval(gameInterval)
+                const restart = confirm(`
+                Игра закончена! Ваш счет: ${this.user.score}
+                Сыграть еще раз?
+                `);
+                if(restart) this.start();
+                else this.end();
+            };
+        }, 1000);
+    }
+    end() {
+        this.fishList = [];
+        alert('Игра закончена');
+    }
+    createFish(n) {
+        let newFishes = [];
+        for(let i = 1; i <= n; i++) {
+            let fishNode = document.createElement('div');
+            fishNode.classList.add('fish');
+            console.log(fishNode);
+            console.log(fishNode.offsetHeight);
+            fishNode.style.top = (Math.random() * this.field.pool_H - fishNode.clientWidth) + 'px';
+            fishNode.style.left = (Math.random() * this.field.pool_W - fishNode.clientHeight) + 'px';
+            newFishes.push(fishNode);
+            this.field.pool.append(fishNode);
+            fishNode.onclick = () => {
+                fishNode.remove();
+                this.userscore++;
+                this.field.userscore.innerHTML = `Ваш счет: ${++this.user.score}`;
+            };
+        }
+        return newFishes;
+    }
 }
 
-function main() {
-    let timer = 10000;
-
-    const user = {
-        name: prompt('Введите имя пользователя'),
-        score: 0,
-    }
-
-    field.username.innerHTML = `Имя: ${user.name ? user.name : 'неизвестный'}`;
-    field.userscore.innerHTML = `Ваш счет: ${user.score}`;
-
-    const gameInterval = setInterval(() => {
-        field.timer.innerHTML = `До конца: ${timer / 1000} сек.`;
-        createFish(3);
-        timer -= 1000;
-        if(!timer) {
-            clearInterval(gameInterval)
-            confirm(`
-            Игра закончена! Ваш счет: ${user.score}
-            Сыграть еще раз?
-            `)
-        };
-    }, 1000);
-}
-
-main();
+const newGame = new fishingGame();
+newGame.start();
